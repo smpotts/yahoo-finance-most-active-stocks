a5de339311a5a523f8e48a6ec07b26499d58c5a8
spotts685 <spotts685@gmail.com>
2024-02-05 12:01:48 -0700

    first tab finished

--- src/app.py
+++ src/app.py
@@ -1,8 +1,3 @@
-from io import StringIO
-
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 import dash_mantine_components as dmc
 import dash_bootstrap_components as dbc
 import src.scrape as scrape
@@ -13,36 +8,15 @@ import dash
 from dash import Dash, html, dcc, Input, Output
 from plotly.subplots import make_subplots
 
-
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-stocks = scrape.scrape_data()
+stocks = scrape.clean_trending_ticker_data()
 
-# Create a bar chart for the volume of the most active stocks
-# fig_volume = px.bar(most_active_stocks, x='Name', y='Volume', title='Volume of Most Active Stocks')
-# fig_volume.update_layout(plot_bgcolor='#111', paper_bgcolor='#111', font_color='white')
-# fig_volume.show()
-
-# fig = make_subplots(rows=1, cols=1, subplot_titles=['% Change'])
-
-# % Change visualization
-# fig_change = px.bar(most_active_stocks, x='Name', y='% Change', text='% Change')
-# for trace in fig_change['data']:
-#     fig.add_trace(trace, row=1, col=1)
-
-# fig.update_layout(showlegend=False, plot_bgcolor='#111', paper_bgcolor='#111', font_color='white')
-# fig.update_traces(marker_color='skyblue', marker_line_color='white', marker_line_width=1.5, opacity=0.6)
-# fig.show()
-
-# fig_percent_change = px.bar(most_active_stocks, x='Name', y='% Change', text='% Change')
-# fig_percent_change.update_layout(showlegend=False, plot_bgcolor='#111', paper_bgcolor='#111', font_color='white', yaxis=dict(range=[-10, 10]))
-# fig_percent_change.update_traces(marker_color='skyblue', marker_line_color='white', marker_line_width=1.5, opacity=0.6)
-# fig_percent_change.show()
 
 app.layout = dbc.Container(
     [
         dcc.Store(id="store"),
-        html.H1("Yahoo Finance Most Active Stocks"),
+        html.H1("Yahoo Finance Stocks"),
         html.Hr(),
         dbc.Button(
             "Regenerate graphs",
@@ -52,11 +26,11 @@ app.layout = dbc.Container(
         ),
         dbc.Tabs(
             [
-                dbc.Tab(label="% Change", tab_id="pct_change"),
+                dbc.Tab(label="Trending Tickers", tab_id="trending_tickers"),
                 dbc.Tab(label="Price (Intraday)", tab_id="price"),
             ],
             id="tabs",
-            active_tab="pct_change",
+            active_tab="trending_tickers",
         ),
         html.Div(id="tab-content", className="p-4"),
     ]
@@ -67,26 +41,38 @@ app.layout = dbc.Container(
     Output("tab-content", "children"),
     [Input("tabs", "active_tab"), Input("store", "data")],
 )
-def render_tab_content(active_tab, data):
+def render_tab_content(active_tab):
     """
     This callback takes the 'active_tab' property as input, as well as the
     stored graphs, and renders the tab content depending on what the value of
     'active_tab' is.
     """
-    # if active_tab and data is not None:
-    if active_tab == "pct_change":
-        return dcc.Graph(figure=px.bar(stocks, x='Name', y='% Change').update_traces(marker_color=stocks["Color"]),
-                         style={'height': '100vh'})
+    if active_tab == "trending_tickers":
+        return (
+            [
+                dcc.Graph(figure=px.bar(stocks, x='Name', y='% Change', color='% Change', title="Trending Tickers",
+                                        color_continuous_scale='Bluered')
+                          .update_layout(showlegend=False),
+                          style={'height': '85vh'})
+                # dcc.Graph(figure=px.bar(stocks.where(stocks['% Change'] >= 0), x='Name', y='% Change',
+                #                             size='Market Cap',
+                #                             hover_name="Name", title="Postitive % Change")
+                #           .update_traces(marker_color=stocks["Color"]),
+                #           style={'height': '85vh'}),
+                # dcc.Graph(figure=px.bar(stocks.where(stocks['% Change'] < 0), x='Name', y='% Change',
+                #                             hover_name="Name",
+                #                             size='Market Cap',
+                #                             title="Negative % Change").update_traces(marker_color=stocks["Color"]),
+                #           style={'height': '85vh'}),
+            ])
     elif active_tab == "price":
         return dbc.Row(
-                [
-                    dbc.Col(dcc.Graph(figure=px.bar()), width=6),
-                    # dbc.Col(dcc.Graph(figure=data["hist_2"]), width=6),
-                ]
-            )
-    # return "No tab selected"
+            [
+                dbc.Col(dcc.Graph(figure=px.bar()), width=6),
+                # dbc.Col(dcc.Graph(figure=data["hist_2"]), width=6),
+            ]
+        )
 
 
 if __name__ == '__main__':
-    # scrape.scrape_data()
     app.run(debug=True)
--- src/scrape.py
+++ src/scrape.py
@@ -2,25 +2,54 @@ from io import StringIO
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
 
 
-def scrape_data(url='https://finance.yahoo.com/most-active/?offset=0&count=100'):
-    # Send a GET request
+def clean_trending_ticker_data():
+    # get the trending ticker data into a DataFrame
+    trending_df = scrape_table_data('trending-tickers')
+    # change numeric columns into ints
+    trending_df['% Change'] = trending_df['% Change'].str.replace('%', '').astype(float)
+    trending_df['Market Cap'] = trending_df['Market Cap'].apply(convert_to_int)
+    trending_df['Volume'] = trending_df['Volume'].apply(convert_to_int)
+    return trending_df
+
+
+def convert_to_int(value):
+    value = str(value)
+    # define multipliers
+    multiplier = {'M': 1e6, 'B': 1e9, 'T': 1e12}
+
+    # extract the numeric part and suffix if not null
+    if value == "nan":
+        result = 0
+    else:
+        numeric_part, suffix = value[:-1], value[-1]
+
+        # convert to integer after multiplying by the corresponding multiplier
+        result = int(float(numeric_part) * multiplier.get(suffix, 1))
+    return result
+
+
+def scrape_table_data(path):
+    global df
+    url = f'https://finance.yahoo.com/{path}/'
+
+    # send a GET request to the URL
     response = requests.get(url)
 
-    # Parse the page content
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # parse the HTML content of the page
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find the table
-    table = soup.find('table')
+        # find the table with the class 'W(100%)'
+        table = soup.find('table', class_='W(100%)')
 
-    # Parse the table with pandas
-    dfs = pd.read_html(StringIO(str(table)))
-    most_active_stocks = dfs[0]
-    most_active_stocks['% Change'] = most_active_stocks['% Change'].str.replace('%', '').astype(float) / 100
-    most_active_stocks["Color"] = np.where(most_active_stocks["% Change"] < 0, 'red', 'green')
+        # read the table into a pandas DataFrame
+        df = pd.read_html(StringIO(str(table)))[0]
+    else:
+        print(f"Error: Failed to fetch the page {url}. Status code: {response.status_code}")
 
-    return most_active_stocks
+    return df
 
 
